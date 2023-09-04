1a19b9d14546e18da1c10bcaccf0d96dd4ed3bb7
stwvwe <143316472+stwvwe@users.noreply.github.com>
2023-09-04 10:16:45 +0530

    Add files via upload

---
+++ commands/stats.js
@@ -0,0 +1,6 @@
+module.exports.run = (client, message) => {
+  return message.reply("See \`guildconfig.json\` And Fill It.").then(msg => setTimeout(() => {
+          msg.delete()
+          message.delete()
+        }, client.config.message_remove_time*1000))
+}
